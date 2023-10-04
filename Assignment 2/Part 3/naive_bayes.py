38a5521cafcdf21a9a5ab4e4d654bfd2556afe7e
imhess <79474657+imhess@users.noreply.github.com>
2023-10-03 18:45:14 -0700

    Update to assignment

--- Assignment 2/Part 3/naive_bayes.py
+++ Assignment 2/Part 3/naive_bayes.py
@@ -19,35 +19,71 @@ train = []
 test = []
 
 #reading the training data in a csv file
-with open('weather_training.csv', 'r') as csvfile:
+with open('C:\\Users\\thoma\\Documents\\GitHub\\Machine-Learning-Assignments\\Assignment 2\\Part 3\\weather_training.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
   for i, row in enumerate(reader):
       if i > 0: #skipping the header
-         train.append (row)
+         train.append(row)
 
 #transform the original training features to numbers and add them to the 4D array X.
 #For instance Sunny = 1, Overcast = 2, Rain = 3, so X = [[3, 1, 1, 2], [1, 3, 2, 2], ...]]
-#--> add your Python code here
-# X =
+X = []
+for row in train:
+  temp = []
+  temp.clear()
+  for j, value in enumerate(row):
+    if j == 1:
+      if value == 'Sunny':
+        temp.append(1) 
+      elif value == 'Overcast':
+        temp.append(2)
+      else:
+        temp.append(3)
+    if j == 2:
+      if value == 'Hot':
+        temp.append(1) 
+      elif value == 'Mild':
+        temp.append(2)
+      else:
+        temp.append(3)
+    if j == 3:
+      if value == 'High':
+        temp.append(1) 
+      else:
+        temp.append(2)
+    if j == 4:
+      if value == 'Strong':
+        temp.append(1) 
+      else:
+        temp.append(2)
+  X.append(temp)
 
 #transform the original training classes to numbers and add them to the vector Y.
 #For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
 #--> add your Python code here
-# Y =
+Y = []
+for row in train:
+  if row[-1] == 'Yes':
+    Y.append(1)
+  else:
+    Y.append(2)
 
 #fitting the naive bayes to the data
 clf = GaussianNB()
 clf.fit(X, Y)
 
 #reading the test data in a csv file
-with open('weather_test.csv', 'r') as csvfile:
+with open('C:\\Users\\thoma\\Documents\\GitHub\\Machine-Learning-Assignments\\Assignment 2\\Part 3\\weather_test.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
   for i, row in enumerate(reader):
       if i > 0: #skipping the header
          test.append (row)
+      else:
+        heading = row
 
 #printing the header of the solution
-#--> add your Python code here
+for label in heading:
+  print(label, end='  ')
 
 #use your test samples to make probabilistic predictions. For instance: clf.predict_proba([[3, 1, 2, 1]])[0]
 #--> add your Python code here

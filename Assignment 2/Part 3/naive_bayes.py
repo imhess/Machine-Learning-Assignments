a3b68855455af20ea0bd7ba73ec6472c1b9a1e8e
imhess <79474657+imhess@users.noreply.github.com>
2023-10-03 17:14:09 -0700

    Init

--- Assignment 2/Part 3/naive_bayes.py
+++ Assignment 2/Part 3/naive_bayes.py
@@ -1,9 +1,11 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Isaiah Hessler
+# FILENAME: Assignment 2 - Part 3
+# SPECIFICATION: Complete the Python program (naïve_bayes.py) that will read the file
+# weather_training.csv (training set) and output the classification of each test instance from the file
+# weather_test (test set) if the classification confidence is >= 0.75.
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
@@ -11,9 +13,17 @@
 
 #importing some Python libraries
 from sklearn.naive_bayes import GaussianNB
+import csv
+
+train = []
+test = []
 
 #reading the training data in a csv file
-#--> add your Python code here
+with open('weather_training.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0: #skipping the header
+         train.append (row)
 
 #transform the original training features to numbers and add them to the 4D array X.
 #For instance Sunny = 1, Overcast = 2, Rain = 3, so X = [[3, 1, 1, 2], [1, 3, 2, 2], ...]]
@@ -30,9 +40,13 @@ clf = GaussianNB()
 clf.fit(X, Y)
 
 #reading the test data in a csv file
-#--> add your Python code here
+with open('weather_test.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0: #skipping the header
+         test.append (row)
 
-#printing the header os the solution
+#printing the header of the solution
 #--> add your Python code here
 
 #use your test samples to make probabilistic predictions. For instance: clf.predict_proba([[3, 1, 2, 1]])[0]
